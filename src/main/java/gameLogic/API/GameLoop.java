5f8c4f81b4767b1f1bfd3527ab631e6e6e59722a
FilippoBadioli <filippo.badioli@studio.unibo.it>
2023-03-14 17:10:16 +0100

    added GameLoop.java with an unfinished implementation

---
+++ src/main/java/gameLogic/API/GameLoop.java
@@ -0,0 +1,22 @@
+import java.time.Clock;
+
+public class GameLoop implements Runnable{
+
+    Clock clock;
+    double previousTime = clock.millis();
+    
+    @Override
+    public void run() {
+        while (true /*!GameLogic.isGameEnd()*/) {
+            double currentTime = clock.millis();
+            double timeDifference = currentTime - previousTime;
+            //inputs
+            //update
+            //output
+            previousTime = currentTime;
+        }
+    }
+
+
+
+}
