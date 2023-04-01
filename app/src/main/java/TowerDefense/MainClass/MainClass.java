b3e3bbecb621cdb46d356d214ffcd00eb82904e1
FilippoBadioli <filippo.badioli@studio.unibo.it>
2023-04-01 11:33:40 +0200

    Fixing time getters in gameloop

--- app/src/main/java/TowerDefense/MainClass/MainClass.java
+++ app/src/main/java/TowerDefense/MainClass/MainClass.java
@@ -5,9 +5,7 @@ import TowerDefense.gameLogic.Impl.GameLoop;
 
 public class MainClass {
     public static void main(String[] args) {
-        //new Board(size);
-        //GameLoop gameLoop = new GameLoop();
-        //gameLoop.run();
+        new GameLoop();
         new Game();
     }
 }
--- app/src/main/java/TowerDefense/gameLogic/Impl/GameLoop.java
+++ app/src/main/java/TowerDefense/gameLogic/Impl/GameLoop.java
@@ -1,20 +1,23 @@
 package TowerDefense.gameLogic.Impl;
 import java.awt.Point;
-import java.time.Clock;
+import java.lang.System;
 import java.util.Map;
 import TowerDefense.Entities.API.MovingEntity;
 import TowerDefense.Entities.Impl.TowerSingleton;
 
-public class GameLoop implements Runnable{
+public class GameLoop extends Thread{
 
-    private Clock clock;
-    private double previousTime = clock.millis();
+    private double previousTime = System.currentTimeMillis();
     private final double FIXED_TIME = 1/60;
+
+    public GameLoop() {
+        this.run();
+    }
     
     @Override
     public void run() {
         while (TowerSingleton.getInstance().getHp() > 0) {
-            double currentTime = clock.millis();
+            double currentTime = System.currentTimeMillis();
             double timeDifference = currentTime - previousTime;
             update(timeDifference);
             previousTime = currentTime;
@@ -25,7 +28,7 @@ public class GameLoop implements Runnable{
         Map<MovingEntity, Point> entityPositions = GameLogicImpl.getPositions();
         //Draw.draw(entityPositions);
         try {
-            this.wait((long)(FIXED_TIME - timeDifference));
+            GameLoop.sleep((long)(FIXED_TIME - timeDifference));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
