411552c1bddbca7ae946d3bede9146b08eeb410a
Giovanni Minoccari <minoccarigiovanni@gmail.com>
2023-05-08 15:48:56 +0200

    AI changed

--- app/src/main/java/towerDefense/entities/impl/Turret.java
+++ app/src/main/java/towerDefense/entities/impl/Turret.java
@@ -9,9 +9,14 @@ public class Turret extends RangedEntity {
     static int cost = 200;
 
     public Turret() {
-        super(new Point(120, 400), 0, 50000, 500, Constants.turret, cost);
+        super(new Point(120, 400), 1, 8000, 500, Constants.turret, cost);
         super.resizeRangebox(500, 200);
         super.resizeHitbox(20, 200);
+    }
+
+    @Override
+    public void updatePosition(){
+        
     }
 
     public static int getCost() {
